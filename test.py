72b9db3588aa5125ab84ca9fa49b7c568873fe49
Umair-Mubeen <106667150+Umair-Mubeen@users.noreply.github.com>
2024-05-22 13:10:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,25 @@ If the columns of the two DataFrames are different and you want to concatenate t
 
 Here's how you can do it:
 
+ meor_columns = ['Order Event', 'Fix_Col_0', 'FirmROID', 'MsgType', 'CAT_IM_ID', 'Date', 'Order ID',
+                            'Symbol',
+                            'Fix_Col_1', 'TimeStamp', 'Fix_Col_2', 'Fix_Col_3', 'Fix_Col_4', 'Fix_Col_5', 'SideType',
+                            'Price',
+                            'Quantity', 'Fix_Col_6', 'OrderType', 'TIF', 'Trading_Session', 'FDID', 'Fix_Col_7',
+                            'Fix_Col_8',
+                            'Fix_Col_9', 'Fix_Col_10', 'Fix_Col_11', 'Fix_Col_12', 'Fix_Col_13', 'Fix_Col_14',
+                            'Fix_Col_15',
+                            'Fix_Col_16']
+
+            meno_columns = ['Order Event', 'Fix_Col_0', 'FirmROID', 'MsgType', 'CAT_IM_ID',
+                       'Date', 'Order ID', 'Symbol', 'TimeStamp', 'Fix_Col_1', 'Fix_Col_2', 'Fix_Col_3', 'Fix_Col_4',
+                       'Fix_Col_5', 'Fix_Col_6', 'Fix_Col_7', 'Fix_Col_8', 'SideType', 'Price', 'Quantity', 'Fix_Col_9',
+                       'OrderType', 'TIF', 'Trading_Session', 'Fix_Col_10', 'Fix_Col_11', 'FDID', 'Acc Type',
+                       'Fix_Col_12',
+                       'Fix_Col_13', 'Fix_Col_14', 'Fix_Col_15', 'Fix_Col_16', 'Fix_Col_17', 'Fix_Col_18', 'Fix_Col_19',
+                       'Fix_Col_20', 'Fix_Col_21', 'Fix_Col_22', 'Fix_Col_23', 'Fix_Col_24', 'Fix_Col_25', 'Fix_Col_26',
+                       'Fix_Col_27', 'Fix_Col_28', 'Fix_Col_29']
+
 ```python
 import pandas as pd
 
