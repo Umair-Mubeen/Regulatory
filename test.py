07324a9f6ec4200aa2bbd95d8d4339cbc7b5862d
Umair-Mubeen <106667150+Umair-Mubeen@users.noreply.github.com>
2024-05-23 13:06:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
-def readCSV():
-    try:
-        with open('MENO_Creation.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            with open('MEOR_Creation.csv', 'r') as f:
-                reader = csv.reader(f)
-                next(reader, None)
-                for row in reader:
-                    writer.writerow(row)
-        downloadCSV(f)
-        pass
-    except Exception as e:
-        print("Merge CSV Exception :-" + str(e))
+import os
+import glob
+import json
+
+try:
+    result = []
+    directory = "C:\\Users\\umair\\Downloads\\VCIN CAT APRIL\\VCIN CAT APRIL\\95049_VCIN_20240425_OrderEvents_000101.json"
+    json_pattern = os.path.join(directory, '*.json')
+    print(json_pattern)
+    for f in glob.glob(json_pattern):
+        with open(f, "rb") as infile:
+            result.append(json.load(infile))
+
+    with open("merged_file.json", "wb") as outfile:
+        json.dump(result, outfile, indent=4)
+except Exception as e:
+    print("Error while merging files Json :" + str(e))
